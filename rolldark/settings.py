a2c1f36e34b46f8994a67fcfddf81a26508baf6e
Ben Miles <benmilesGM@gmail.com>
2025-06-10 14:44:22 +0100

    adds EMAIL_BACKEND to settings.py

--- rolldark/settings.py
+++ rolldark/settings.py
@@ -132,3 +132,5 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
