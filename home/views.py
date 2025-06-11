8dccf4631241ae4fe453955e33c072692051b6bd
Ben Miles <benmilesGM@gmail.com>
2025-06-11 19:53:58 +0100

    adds index() to home/views.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
+"""Home app views"""
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """View to return index page"""
+
+    return render(request, 'home/index.html')
