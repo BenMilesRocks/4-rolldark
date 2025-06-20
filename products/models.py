20fcebfc7fbb18921fdd073e1ec9ec4fd2371e59
Ben Miles <benmilesGM@gmail.com>
2025-06-20 13:17:45 +0100

    adds Product to products/models.py

--- products/models.py
+++ products/models.py
@@ -18,3 +18,16 @@ class Category(models.Model):
     def get_friendly_name(self):
         '''return display name'''
         return str(self.friendly_name)
+
+class Product(models.Model):
+    '''Top level Product model'''
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    delivery_charge = models.BooleanField(null=True, blank=True, default=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
