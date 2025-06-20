16e9f237bd5de80db682e9de895bcb1a03608904
Ben Miles <benmilesGM@gmail.com>
2025-06-20 13:17:05 +0100

    adds Category class to products/models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
+'''Products app models'''
+
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    '''Top Level Category model'''
+
+    class Meta:
+        '''Fixes the pluralisation on the Admin page, shows Categories instead of Categorys'''
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_friendly_name(self):
+        '''return display name'''
+        return str(self.friendly_name)
