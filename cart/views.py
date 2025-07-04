e5290f4a8faef7bdee2c412a0111004635407558
Ben Miles <benmilesGM@gmail.com>
2025-07-04 11:49:44 +0100

    corrects typo, refactor add_to_cart()

--- cart/views.py
+++ cart/views.py
@@ -8,28 +8,28 @@ def view_cart(request):
     '''Returns cart page'''
     return render(request, 'cart/cart.html')
 
-def add_to_bag(request, item_id):
-    '''Adds items to the bag'''
+def add_to_cart(request, item_id):
+    '''Adds items to the cart'''
 
     # Fetch variables from page
     product = Product.objects.get(pk=item_id) #pylint: disable=E1101
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
-    # If bag exists in session fetches it, else create empty bag
-    bag = request.session.get('bag', {})
+    # If cart exists in session fetches it, else create empty cart
+    cart = request.session.get('cart', {})
 
-    # Check if already in bag
-    if item_id in list(bag.keys()):
-        # If in bag, increment quantity
-        bag[item_id] += quantity
+    # Check if already in cart
+    if item_id in list(cart.keys()):
+        # If in cart, increment quantity
+        cart[item_id] += quantity
     else:
-        # If not, add new key to bag
-        bag[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your bag')
+        # If not, add new key to cart
+        cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your cart')
 
-    # Pushes bag back to session
-    request.session['bag'] = bag
+    # Pushes cart back to session
+    request.session['cart'] = cart
 
     # Redirect to last page visited
     return redirect(redirect_url)
