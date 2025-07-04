80064e561661d7e52559ebbad738a82553d4f8c3
Ben Miles <benmilesGM@gmail.com>
2025-07-04 11:08:59 +0100

    adds view_cart() to cart/views.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,6 @@
+'''Cart app views'''
 from django.shortcuts import render
 
-# Create your views here.
+def view_cart(request):
+    '''Returns cart page'''
+    return render(request, 'cart/cart.html')
