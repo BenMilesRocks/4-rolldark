1cae06509d39844fa93b73cba4f03821b45b5e7f
Ben Miles <benmilesGM@gmail.com>
2025-07-05 14:33:51 +0100

    fixes urls for dice_detail.html

--- dice/urls.py
+++ dice/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_dice, name='dice'),    
-    path('<product_id>', views.dice_detail, name='dice_detail')
+    path('', views.all_dice, name='dice'),
+    path('<dice_id>', views.dice_detail, name='dice_detail')
 ]
