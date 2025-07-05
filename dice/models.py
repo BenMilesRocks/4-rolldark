4b0b45cde68ad47c05bebee23ce1f2777d5a2614
Ben Miles <benmilesGM@gmail.com>
2025-07-05 13:18:12 +0100

    adds Dice to dice/models.py

--- dice/models.py
+++ dice/models.py
@@ -18,3 +18,20 @@ class Category(models.Model):
     def get_friendly_name(self):
         '''return display name'''
         return str(self.friendly_name)
+
+class Dice(models.Model):
+    '''Top level Dice model'''
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, unique=True, null=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    product_live = models.BooleanField(default=False)
+
+    # Dice specific fields
+    delivery_charge = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.name)
