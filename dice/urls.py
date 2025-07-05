325217e4f2af6bf6d937e4067cb751f1f94b907e
Ben Miles <benmilesGM@gmail.com>
2025-07-05 14:28:11 +0100

    adds dice_detail to dice/urls.py

--- dice/urls.py
+++ dice/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_dice, name='dice'),
+    path('', views.all_dice, name='dice'),    
+    path('<product_id>', views.dice_detail, name='dice_detail')
 ]
