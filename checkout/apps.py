ce57199f58002a96fe733e46b8ab09884160017f
Ben Miles <benmilesGM@gmail.com>
2025-07-17 13:28:29 +0100

    adds ready() to CheckoutCongig in checkout/apps.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,11 @@
+'''Checkout apps.py'''
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    '''Config for checkout app'''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        import checkout.signals #pylint: disable= W0611, C0415
