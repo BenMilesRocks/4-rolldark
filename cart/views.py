c1214e6af8a2767f61efe20b5d0ef265f94fcab5
Ben Miles <benmilesGM@gmail.com>
2025-07-22 21:44:49 +0100

    adds ticket_option to add_game_to_cart()

--- cart/views.py
+++ cart/views.py
@@ -45,17 +45,26 @@ def add_game_to_cart(request, item_id):
     product = Product.objects.get(pk=item_id) #pylint: disable=E1101
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    if request.POST.get(f'option_{product}') == 'campaign_ticket':
+        ticket_option = 'campaign_ticket'
+    else:
+        ticket_option = request.POST.get(f'option_{product}')
 
     # If cart exists in session fetches it, else create empty cart
     cart = request.session.get('cart', {})
 
     # Check if already in cart
     if item_id in list(cart.keys()):
-        # If in cart, increment quantity
-        cart[item_id] += quantity
+        # If in cart, check if ticket option in cart
+        if ticket_option in cart[item_id]['game_by_ticket_option'].keys():
+            # If in cart, increment quantity
+            cart[item_id]['game_by_ticket_option'][ticket_option] += quantity
+        else:
+            # Else add ticket option to game ID
+            cart[item_id]['game_by_ticket_option'][ticket_option] = quantity
     else:
         # If not, add new key to cart
-        cart[item_id] = quantity
+        cart[item_id] = {'game_by_ticket_option': {ticket_option: quantity}}
         messages.success(request, f'Added {product.name} to your cart')
 
     # Pushes cart back to session
